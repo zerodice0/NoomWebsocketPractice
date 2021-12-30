4b583b0b092bcef07ad0c51170ec48bfb7a91530
zerodice0 <zerodice0@gmail.com>
2021-12-30 09:38:38 +0900

    - distinguish nickname-form and message-form. - add handleNicknameSubmit funcion. - add nickname to welcom, bye, message handler. - add nicknameChanged handler.

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -12,15 +12,30 @@ const showRoom = () => {
   welcome.hidden = true;
   room.hidden = false;
   const h3 = room.querySelector("h3");
-  const form = room.querySelector("form");
+  const messageForm = room.querySelector("#message");
+  const nicknameForm = room.querySelector("#nickname");
   h3.innerHTML = `Current Room: ${roomName}`;
-  form.addEventListener("submit", handleMessageSubmit);
+  messageForm.addEventListener("submit", handleMessageSubmit);
+  nicknameForm.addEventListener("submit", handleNickanmeSubmit);
+}
+
+const handleNickanmeSubmit = (event) => {
+  event.preventDefault();
+  const nicknameForm = room.querySelector("#nickname");
+  const input = nicknameForm.querySelector("input");
+  const nickname = input.value;
+  socket.emit(
+    "nickname",
+    nickname,
+    () => sendMessage(`You are now known as ${nickname}`)
+  );
+  input.value = "";
 }
 
 const handleMessageSubmit = (event) => {
   event.preventDefault();
-  const form = room.querySelector("form");
-  const input = form.querySelector("input");
+  const messageForm = room.querySelector("#message");
+  const input = messageForm.querySelector("input");
   const message = input.value;
   socket.emit(
     "message",
@@ -51,10 +66,19 @@ const sendMessage = (message) => {
   ul.appendChild(li);
 }
 
-socket.on("welcome", () => sendMessage("Someone joined to room!"));
-socket.on("bye", (reason) => sendMessage(
-  `Someone left the room!${ reason ? ` Reason: ${reason}` : ""}` 
+socket.on("welcome", (nickname) => sendMessage(`Someone joined to room! ${nickname ?? ""}`));
+socket.on("bye", (reason, nickname) => sendMessage(
+  `${nickname ?? "Someone"} left the room!${ reason ? ` Reason: ${reason}` : ""}` 
 ));
-socket.on("message", (message) => sendMessage(message));
+socket.on(
+  "message",
+  (message, nickname) => 
+    sendMessage(`${nickname ?? "unknown"}: ${message}`)
+);
+socket.on(
+  "nicknameChanged",
+  (oldNickname, newNickname) =>
+    sendMessage(`${oldNickname ?? "unknown"} is now known as ${newNickname}`)
+);
 
 form.addEventListener("submit", handleRoomSubmit);
