8a3ed54f1fb9f8fa5d9058362da5fa466eda9e45
zerodice0 <zerodice0@gmail.com>
2021-12-30 21:47:41 +0900

    add interface EnterRoomMessage.

--- src/models/message.ts
+++ src/models/message.ts
@@ -4,6 +4,13 @@ export interface Message {
   payload: string;
 }
 
+export interface EnterRoomMessage {
+  payload: {
+    nickName: string;
+    roomName: string;
+  }
+}
+
 export interface NamedSocket {
   webSocket:SocketIO.Socket;
   nickname:string | null;
